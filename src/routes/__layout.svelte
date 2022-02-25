3429806cbb8080dae761ed3105d04bebecc4cdb4
Nick Jessop <nick@parachutelabs.ca>
2022-02-25 10:51:00 -0800

    Adds tailwind and fonts

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  // import Header from "$lib/Header/index.svelte"
+  import "../app.css"
+  import "../styles/fonts.css"
+</script>
+
+<!-- <Header /> -->
+
+<main>
+  <slot />
+</main>
+
+<footer>
+  <p>
+    footer
+  </p>
+</footer>
---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		fontFamily: {
+			sans: ['Metropolis-Regular'],
+			serif: ['ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
+			mono: [
+				'ui-monospace',
+				'SFMono-Regular',
+				'Menlo',
+				'Monaco',
+				'Consolas',
+				'Liberation Mono',
+				'Courier New',
+				'monospace'
+			]
+		},
+		extend: {
+			fontSize: {
+				'10xl': '9rem',
+				'11xl': '12rem',
+				'12xl': '15rem'
+			}
+		}
+	},
+	plugins: [require('@tailwindcss/typography')]
+};
