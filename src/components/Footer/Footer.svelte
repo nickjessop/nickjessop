c1f166f2861b8e2af4e395ad8970d4d5626ff9f4
Nick Jessop <nick.jessop@icloud.com>
2023-07-09 12:53:53 -0400

    Adds js to fetch the current year

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -1,3 +1,7 @@
+<script>
+	const year = new Date().getFullYear();
+</script>
+
 <footer class="mt-24 p-4 text-xs w-full text-center text-gray-600 mono-font max-w-7xl">
-	&copy; NICK JESSOP 2022
+	&copy; NICK JESSOP {year}
 </footer>
